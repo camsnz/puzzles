93cebe05749f079e1752b5be71985fa7600a4c85
Cam Scott <cameronscott@gmail.com>
2024-02-15 13:18:03 +1300

    add 0797-all-paths-from-source-to-target

---
+++ leetcode/0797-all-paths-from-source-to-target/all-paths.py
@@ -0,0 +1,35 @@
+# Given a directed acyclic graph (DAG) of n nodes labeled from 0 to n - 1, find all possible paths from node 0 to node n - 1 and return them in any order.
+# The graph is given as follows: graph[i] is a list of all nodes you can visit from node i (i.e., there is a directed edge from node i to node graph[i][j]).
+
+# Input: graph = [[1,2],[3],[3],[]]
+# Output: [[0,1,3],[0,2,3]]
+# Explanation: There are two paths: 0 -> 1 -> 3 and 0 -> 2 -> 3.
+
+from ast import List
+
+
+class Solution:
+
+    def filterOut(node, visited):
+        return [i for i in node if i not in visited]
+
+    def visit(self, graph, idx=0, visited=[]):
+        visited = visited.copy()
+        visited.append(idx)
+        if idx == len(graph)-1:
+            return [visited]
+
+        options = self.filterOut(graph[idx], visited)
+        if not options:
+            return []
+
+        results = []
+        for o in options:
+            vr = self.visit(graph, o, visited)
+            for path in vr:
+                if path:
+                    results.append(path)
+        return results
+
+    def allPathsSourceTarget(self, graph: List[List[int]]) -> List[List[int]]:
+        return self.visit(graph)
